c8bc48f1b82bfbc897e1eca272d83f5ed12770b0
Gemechisgk <gemechis.gk@gmail.com>
2022-06-02 23:01:33 +0300

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,45 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
+#include "holberton.h"
 
-	n = _atoi("xd-xds5");
-	/*n = 1000000000;*/
-	printf("%d\n", n);
-	n = _atoi("-402");
-	printf("%d\n", n);
-	n = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", n);
-	n = _atoi("214748364");
-	printf("%d\n", n);
-	n = _atoi("0");
-	printf("%d\n", n);
-	n = _atoi("Suite 402");
-	printf("%d\n", n);
-	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	printf("%d\n", n);
-	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", n);
-	return (0);
+/**
+ * is_numerical - Check if is a digit
+ * @n: Number
+ * Return: If is a number, return 1 else return 0
+ */
+int is_numerical(unsigned int n)
+{
+	return (n >= '0' && n <= '9');
+}
+
+/**
+ * _atoi - Convert a string to a number
+ * @s: String
+ * Return: Return the num
+ */
+int _atoi(char *s)
+{
+	unsigned int number, i;
+	int sign;
+
+	sign = 1;
+	number = 0;
+
+	/*Start FOR*/
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_numerical(s[i])) /*Condition*/
+		{
+			number = (s[i] - 48)  + number * 10;
+
+			if (s[i + 1] == ' ')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+
+	}
+
+	return (number * sign);
+
 }
