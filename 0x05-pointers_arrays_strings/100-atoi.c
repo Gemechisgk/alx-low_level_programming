233cdcc570cb35be10c0f86add1f985f84b6c7c0
Gemechisgk <gemechis.gk@gmail.com>
2022-06-02 22:34:38 +0300

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
 /**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	int n;
