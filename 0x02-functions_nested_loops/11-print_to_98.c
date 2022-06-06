98b575767b44ae6b2d06e6357379bbdd48746411
Gemechisgk <gemechis.gk@gmail.com>
2022-06-07 01:47:26 +0300

    clean and shine last push

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,82 @@
 */
 void print_to_98(int n)
 {
-if (n > 98)
-{
-for (; n > 98; n--)
-{
-printf(%d, , n);
-}
-}
-else if (n < 98)
-{
-for (; n < 98; n++)
-{
-printf(%d, , n);
-}
-}
-printf(%dn, n);
+	int end;
+
+	end = 98;
+
+	if (n < end)
+	{
+		for (; n <= end; n++)
+		{
+			if (n == 98)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				break;
+			}
+			else
+			{
+				if (n >= -9 && n <= 9)
+				{
+					if (n < 0)
+					{
+						_putchar('-');
+						_putchar((n * -1) + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+					else if (n >= 0)
+					{
+						_putchar(n + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else if (n >= 100)
+				{
+					_putchar((n / 100) + '0');
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+	else if (n == 0)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar('\n');
+	}
+	else
+	{
+		for (; n >= end; n--)
+		{
+			if (n == 98)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				break;
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	return;
 }
