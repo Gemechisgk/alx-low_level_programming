90ca17703863e92f229462a4b70ab48f2e6e8e37
Gemechisgk <55735419+Gemechisgk@users.noreply.github.com>
2022-06-28 22:55:52 +0300

    static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _memset - function that fills memory with a constant byte.
+*@b: valor a establecer
+*@n: # bytes q establece el vr.
+*@s: puntero al bloque de memoria que va a llenar
+*Return: 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{}
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* _atoi - function
+* @s: its a ptr
+* Return: 0.
+*/
+
+int _atoi(char *s)
+{}
